7540c74e6d8f1b3e3d44f7db6e984474f2bcf593
Just EnginErin <31420768+just-enginerin@users.noreply.github.com>
2023-07-02 15:03:29 -0400

    Exercise 6.3 - Layouts

---
+++ week-6/gpa-calculator-app1/src/app/app.module.ts
@@ -0,0 +1,46 @@
+/*
+  Title: app.module.ts
+  Author: Erin Brady
+  Date: 2 July 2023
+  Description: App Module
+*/
+
+// Import modules
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Import components
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthLayoutComponent,
+    BaseLayoutComponent,
+    NotFoundComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
